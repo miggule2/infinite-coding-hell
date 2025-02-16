adcb272e0c3f3fa3011fc6075284b66abb9d5e02
tttyoung <andrew9874@naver.com>
2025-02-16 14:14:55 +0900

    [boj] 1302_베스트셀러 / 실버4 / 20분

--- tttyoung/week_9/boj_1302_베스트셀러.py
+++ tttyoung/week_9/boj_1302_베스트셀러.py
@@ -10,6 +10,7 @@ for i in range(n):
 bookdic = dict(sorted(bookdic.items())) #사전순으로 미리 정렬
 print(max(bookdic, key=bookdic.get)) #최대 value값을 가지는 key값을 추출
 
+#다른방법 풀이: get을 사용하지 않고 최대값을 찾은 후에 그 최댓값을 value로 가지는 모든 key값들을 list에 넣고 사전순으로 정렬하여 0번째 요소를 print
 '''
 n = int(input())
 bookdic = {}
