0ea53123fa79cc4a73991aaca4b3933d62be28a7
maureen272 <maureen72@naver.com>
2025-03-08 16:38:22 +0900

    Leetcode-3-medium-20min

---
+++ maureen272/week9/Leetcode-347-TopKFrequentElements.py
@@ -0,0 +1,10 @@
+import collections
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
