d0b4c38a69b5a90eb03488fcc1e79ca2edb7ae7c
miggule2 <148771546+miggule2@users.noreply.github.com>
2025-03-09 19:28:13 +0900

    [Leetcode] Filter Restaurants by Vegan-Friendly, Price and Distance / medium / 40분

---
+++ miggule2/week12/Leetcode_1333_FilterRestaurantsbyVeganFriendlyPriceandDistance.java
@@ -0,0 +1,25 @@
+package week12;
+
+public class Leetcode_1333_FilterRestaurantsbyVeganFriendlyPriceandDistance {
+    public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+        HashMap<Integer,Queue<int[]>> map = new HashMap<>();
+        Arrays.sort(restaurants,Comparator.comparingInt(a -> a[0]));
+        for(int i =0; i < restaurants.length; i++){
+            if(!map.containsKey(restaurants[i][1])) map.put(restaurants[i][1], new LinkedList<>());
+            map.get(restaurants[i][1]).add(restaurants[i]);
+        }
+        List<Integer> result = new LinkedList<Integer>();
+
+        ArrayList<Integer> setList = new ArrayList<Integer>(map.keySet());
+        Collections.sort(setList);
+        for(int rating : setList){
+            int size = map.get(rating).size();
+            for(int i = 0; i < size; i++){
+                int[] restaurant = map.get(rating).poll();
+                if( ((veganFriendly == 1 && restaurant[2] == 1) || veganFriendly == 0) && restaurant[3] <= maxPrice && restaurant[4] <= maxDistance ) result.addFirst(restaurant[0]);
+            }
+        }
+
+        return result;
+    }
+}
