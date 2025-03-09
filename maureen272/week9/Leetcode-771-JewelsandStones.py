9ddc31bf88d7cce4cadf124bc310fd04e8504936
maureen272 <maureen72@naver.com>
2025-03-09 13:09:33 +0900

    Leetcode-771-easy-30min

--- maureen272/week9/Leetcode-771-JewelsandStones.py
+++ maureen272/week9/Leetcode-771-JewelsandStones.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
-        alpha = {}
+        alpha = {} # dictionary to store the frequency of each stone
         
-        for i in stones:
-            if i not in alpha:
-                alpha[i] = 1
-            else:
+        for i in stones: # count the frequency of each stone
+            if i not in alpha: # if the stone is not in the dictionary, add it
+                alpha[i] = 1 
+            else: # if the stone is in the dictionary, increment the frequency
                 alpha[i] += 1
         
         cnt = 0
-        for j in jewels:
+        for j in jewels: # count the number of jewels
             if j in alpha:
                 cnt += alpha[j]
         
-        return cnt
+        return cnt # return the number of jewels
