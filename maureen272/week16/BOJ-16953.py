a1412a23f4383b3cebc6b9139fe10dbea2beaa65
maureen272 <maureen72@naver.com>
2025-05-25 00:40:46 +0900

    BOJ-16953-A->B-silver2-30min

---
+++ maureen272/week16/BOJ-16953.py
@@ -0,0 +1,16 @@
+# BOJ-16953-A → B
+def min_operations(A, B):
+    operations = 0
+    while B > A:
+        if B % 2 == 0:
+            B //= 2
+        elif B % 10 == 1:
+            B //= 10
+        else:
+            return -1
+        operations += 1
+    return operations + 1 if B == A else -1
+
+A, B = map(int, input().split())
+
+print(min_operations(A, B))
