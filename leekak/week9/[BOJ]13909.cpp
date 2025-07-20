8b0dae5314bb369f1d89291afdad3120e1b3cc69
HAKJIN LEE <87360749+leekak@users.noreply.github.com>
2025-07-20 10:10:07 +0900

    [BOJ] silver5 / 창문 닫기 / 30분

---
+++ leekak/week9/[BOJ]13909.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    int cnt=0;
+    cin >> N;
+    for(int i=1; i*i<=N; i++) {
+        cnt++;
+    }
+    cout << cnt;
+
+    return 0;
+}
