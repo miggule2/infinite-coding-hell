d015db759d49062248de0466301b8ad541bf1774
HAKJIN LEE <87360749+leekak@users.noreply.github.com>
2025-07-27 09:58:13 +0900

    Create [BOJ]1966.cpp
    [BOJ] silver3 / 프린터 큐 / 30분

---
+++ leekak/week10/[BOJ]1966.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int testn;
+	int n, m;
+	int importance;
+	int count;
+
+	scanf("%d", &testn);
+
+	for (int i = 0; i < testn; i++) {
+		count = 0;
+		scanf("%d %d", &n, &m);
+		
+		priority_queue<int> pq;
+		queue<pair<int, int>> q;
+
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &importance);
+			q.push({ j, importance });
+			pq.push(importance);
+		}
+
+		while (!q.empty()) {
+			int location = q.front().first;
+			int value = q.front().second;
+			q.pop();
+            
+			// 값 비교
+			if (pq.top() == value) {
+				pq.pop();
+				++count;
+				if (m == location) {
+					printf("%d\n", count);
+					break;
+				}
+			}
+			else q.push({ location, value });
+		}
+	}
+	return 0;
+}
