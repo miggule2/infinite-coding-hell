3a467dd641c707ecae58fb2b404df0b36704a932
miggule2 <miggul2@naver.com>
2025-08-10 18:32:04 +0900

    [BOJ] 실버3 / 2xn 타일링 / 50분

--- miggule2/week24/BOJ_11727_2XN타일링.java
+++ miggule2/week24/BOJ_11727_2XN타일링.java
@@ -14,14 +14,22 @@ public class BOJ_11727_2XN타일링 {
         System.out.println(dp[n]);
     }
 
+    // dp 문제
+    // 1. 1,2,3 단계의 경우엔 기본값으로 잡음
+    // 2. 짝수의 경우 각 단계의 직사각형을 반으로 나눠 나온 숫자의 dp값 곱 + 중간을 2*1,2*2 로 잊고 나머지 도형간의 곱을 더한 값을 dp값을 저장
+    // 3. 홀수의 경우 각 단계의 직사각형을 반으로 (n/2,n/2+1)로 나눠 나온 숫자의 dp값 곱 + 중간을 2*1,2*2 로 잊고 나머지 도형간의 곱을 더한 값을 dp값을 저장
+    // 4. recursion(n)의 값을 출력(dp[n])
     public static int recursion(int[] dp, int num){
         if(num < 0 || num > 1000) return 0;
 
-        if(dp[num] != 0) return dp[num];
+        // 이미 있는 값은 그대로 리턴
+        if(dp[num] != 0) return dp[num]; 
         
         if(num%2 == 0){
+            //짝수의 경우
             dp[num] = (recursion(dp,num/2)*recursion(dp,num/2) + recursion(dp,num/2-1)*recursion(dp,num/2-1)*2)%10007;
         } else {
+            //홀수의 경우
             dp[num] = (recursion(dp,num/2)*recursion(dp,num/2+1) + recursion(dp,num/2-1)*recursion(dp,num/2)*2)%10007;
         }
 
