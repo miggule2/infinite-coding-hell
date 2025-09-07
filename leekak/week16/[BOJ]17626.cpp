93a6fbdd4cbf7cd5412e1bf6b37ab1c8c5ff65bc
HAKJIN LEE <87360749+leekak@users.noreply.github.com>
2025-09-07 10:06:25 +0900

    [BOJ] silver3 / Four Squares / 40분

---
+++ leekak/week16/[BOJ]17626.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> dp(50001,0);
+    dp[1] = 1;
+    for(int i=1; i<=n; i++) {
+        dp[i] = dp[1] + dp[i-1];
+        for(int j=2; j*j<=i; j++) {
+            dp[i] = min(dp[i], 1+dp[i-j*j]);
+        }
+    }
+    cout << dp[n];
+    return 0;
+}
