d2a62ef98d13239c4c3aa6e761af66b8b9da8455
maureen272 <maureen72@naver.com>
2025-11-13 01:15:49 +0900

    Leetcode-169-MajorityElemnets-hard-50min

--- maureen272/week29/Leetcode-241-DifferentWaystoAddParentheses.py
+++ maureen272/week29/Leetcode-241-DifferentWaystoAddParentheses.py
@@ -4,30 +4,34 @@ class Solution(object):
         :type expression: str
         :rtype: List[int]
         """
+
+        # 두 리스트(left, right)의 가능한 모든 조합에 대해 연산 수행
         def compute(left, right, op):
-            results = []
+            results = [] 
             for l in left:
                 for r in right:
                     if op == '+':
-                        results.append(l + r)
+                        results.append(l + r)  # 덧셈 결과 추가
                     elif op == '-':
-                        results.append(l - r)
+                        results.append(l - r)  # 뺄셈 결과 추가
                     elif op == '*':
-                        results.append(l * r)
-            return results
+                        results.append(l * r)  # 곱셈 결과 추가
+            return results  # 가능한 모든 결과 리스트 반환
 
-        # If expression is a single number, return it as an integer
+        # expression이 숫자만으로 이루어진 경우
         if expression.isdigit():
-            return [int(expression)]
+            return [int(expression)]  # 숫자로 변환해 리스트로 반환
 
-        results = []
+        results = []  # 가능한 모든 결과 저장용 리스트
+        # expression의 각 문자 순회
         for idx, value in enumerate(expression):
+            # 연산자(+, -, *)인 경우
             if value in "-+*":
-                # Split expression into left and right parts
+                # 연산자를 기준으로 왼쪽, 오른쪽 부분식 분할
                 left = self.diffWaysToCompute(expression[:idx])
                 right = self.diffWaysToCompute(expression[idx+1:])
                 
-                # Compute combinations and extend results
+                # 왼쪽과 오른쪽의 가능한 결과 조합을 계산해 results에 추가
                 results.extend(compute(left, right, value))
                 
-        return results
+        return results  # 모든 가능한 결과 반환
